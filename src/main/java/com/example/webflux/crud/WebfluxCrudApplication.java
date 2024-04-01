1d83a0e47c5606039dec044afb6c9edd0423b926
egonzalez27 <egonzalez@lendingpoint.com>
2024-04-01 09:23:20 -0600

    Initial commit

---
+++ src/main/java/com/example/webflux/crud/WebfluxCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.webflux.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webflux/crud/WebfluxCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webflux.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
